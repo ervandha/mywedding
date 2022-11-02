58c957d7d6673caf1618abba1f45c48b089ed392
Ervandha <46629349+ervandha@users.noreply.github.com>
2022-11-02 23:36:47 +0630

    Update count.js

--- js/count.js
+++ js/count.js
@@ -1,27 +1,61 @@
-//create a javascript timer function
-function timerCountDOwn(date, id) {
-    const el = document.getElementById(id)
-
-    if (!el) return
-    let countDownDate = new Date(date).getTime()
-    const timer = setInterval(function () {
-        const now = new Date().getTime(),
-            distance = countDownDate - now,
-            days = Math.floor(distance / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-            minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
-            seconds = Math.floor((distance % (1000 * 60)) / 1000),
-            //Format your output here
-            output = `<span class="days">${days}D</span> <span class="hours">${hours}H</span> <span class="minutes">${minutes}M</span> <span class="seconds">${seconds}S</span>`
-
-        el.innerHTML = output
-
-        if (distance < 0) {
-            clearInterval(timer)
-            el.innerHTML = 'EXPIRED'
-        }
-    }, 1000)
-}
-
-// then you can use it on multiple element in a single page
-timerCountDOwn('Jan 5, 2023 00:00:00', 'timer1')
+(function (e) {
+  e.fn.countdown = function (t, n) {
+  function i() {
+    eventDate = Date.parse(r.date) / 1e3;
+    currentDate = Math.floor(e.now() / 1e3);
+    if (eventDate <= currentDate) {
+      n.call(this);
+      clearInterval(interval)
+    }
+    seconds = eventDate - currentDate;
+    days = Math.floor(seconds / 86400);
+    seconds -= days * 60 * 60 * 24;
+    hours = Math.floor(seconds / 3600);
+    seconds -= hours * 60 * 60;
+    minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+    days == 1 ? thisEl.find(".timeRefDays").text("day") : thisEl.find(".timeRefDays").text("days");
+    hours == 1 ? thisEl.find(".timeRefHours").text("hour") : thisEl.find(".timeRefHours").text("hours");
+    minutes == 1 ? thisEl.find(".timeRefMinutes").text("minute") : thisEl.find(".timeRefMinutes").text("minutes");
+    seconds == 1 ? thisEl.find(".timeRefSeconds").text("second") : thisEl.find(".timeRefSeconds").text("seconds");
+    if (r["format"] == "on") {
+      days = String(days).length >= 2 ? days : "0" + days;
+      hours = String(hours).length >= 2 ? hours : "0" + hours;
+      minutes = String(minutes).length >= 2 ? minutes : "0" + minutes;
+      seconds = String(seconds).length >= 2 ? seconds : "0" + seconds
+    }
+    if (!isNaN(eventDate)) {
+      thisEl.find(".days").text(days);
+      thisEl.find(".hours").text(hours);
+      thisEl.find(".minutes").text(minutes);
+      thisEl.find(".seconds").text(seconds)
+    } else {
+      alert("Invalid date. Example: 30 Tuesday 2013 15:50:00");
+      clearInterval(interval)
+    }
+  }
+  var thisEl = e(this);
+  var r = {
+    date: null,
+    format: null
+  };
+  t && e.extend(r, t);
+  i();
+  interval = setInterval(i, 1e3)
+  }
+  })(jQuery);
+  $(document).ready(function () {
+  function e() {
+    var e = new Date;
+    e.setDate(e.getDate() + 60);
+    dd = e.getDate();
+    mm = e.getMonth() + 1;
+    y = e.getFullYear();
+    futureFormattedDate = mm + "/" + dd + "/" + y;
+    return futureFormattedDate
+  }
+  $("#countdown").countdown({
+    date: "17 November 2022 21:30:00", // Change this to your desired date to countdown to
+    format: "on"
+  });
+});
