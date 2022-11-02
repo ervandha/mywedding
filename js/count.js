38dbd4df3512d318fd95605fec233fd14a74900d
Ervandha <46629349+ervandha@users.noreply.github.com>
2022-11-02 23:15:41 +0630

    Update count.js

--- js/count.js
+++ js/count.js
@@ -1,41 +1,27 @@
-function getTimeRemaining(endtime) {
-  var t = Date.parse(October 20, 2022 18:00:00") - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
+//create a javascript timer function
+function timerCountDOwn(date, id) {
+    const el = document.getElementById(id)
+
+    if (!el) return
+    let countDownDate = new Date(date).getTime()
+    const timer = setInterval(function () {
+        const now = new Date().getTime(),
+            distance = countDownDate - now,
+            days = Math.floor(distance / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+            seconds = Math.floor((distance % (1000 * 60)) / 1000),
+            //Format your output here
+            output = `<span class="days">${days}D</span> <span class="hours">${hours}H</span> <span class="minutes">${minutes}M</span> <span class="seconds">${seconds}S</span>`
+
+        el.innerHTML = output
+
+        if (distance < 0) {
+            clearInterval(timer)
+            el.innerHTML = 'EXPIRED'
+        }
+    }, 1000)
 }
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
-
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
-
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
-
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+// then you can use it on multiple element in a single page
+timerCountDOwn('Jan 5, 2023 00:00:00', 'timer1')
