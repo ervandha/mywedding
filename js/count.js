115ad7c2f1e49810c4e6154de6cfef096ece65dd
Ervandha <46629349+ervandha@users.noreply.github.com>
2022-11-05 21:21:01 +0630

    Update count.js

--- js/count.js
+++ js/count.js
@@ -1,25 +1,30 @@
-<p id="carasingkat"></p>
-<script>
-// Silahkan anda atur tanggal anda
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-// Hitungan Mundur Waktu Dilakukan Setiap Satu Detik
-var x = setInterval(function() {
-  // Mendapatkan Tanggal dan waktu Pada Hari ini
-  var now = new Date().getTime();
-  //Jarak Waktu Antara Hitungan Mundur
-  var distance = countDownDate - now;
-  // Perhitungan waktu hari, jam, menit dan detik
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  // Tampilkan hasilnya di elemen id = "carasingkat"
-  document.getElementById("carasingkat").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  // Jika hitungan mundur selesai,
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("Carasingkat").innerHTML = "EXPIRED";
-  }
-}, 1000);
-</script>
+function getCounter() {
+  var countDownDate = new Date("Aug 11, 2023 22:00:00").getTime();
+
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    var textDays = document.getElementById("days");
+    var textHours = document.getElementById("hours");
+    var textMinutes = document.getElementById("minutes");
+    var textSeconds = document.getElementById("seconds");
+
+    textDays.innerHTML = days < 10 ? "0" + days : days;
+    textHours.innerHTML = hours < 10 ? "0" + hours : hours;
+    textMinutes.innerHTML = minutes < 10 ? "0" + minutes : minutes;
+    textSeconds.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+
+    if (distance < 0) {
+      clearInterval(x);
+    }
+  }, 1000);
+}
